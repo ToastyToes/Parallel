14dbe7f3620d4be9b3eb12adbd3fd3662bfde70d
grubeb2 <grubeb2@rpi.edu>
2018-05-01 10:01:38 -0400

    bucketSort.c no longer seg faults

--- bucketSort.c
+++ bucketSort.c
@@ -6,7 +6,7 @@
 #include <limits.h>
 #include <unistd.h>
 
-#define NUM_MAX 100
+#define NUM_MAX RAND_MAX
 
 int mpi_rank, mpi_size;
 
@@ -17,13 +17,13 @@ struct Bucket {
 	int num;
 };
 
-void sortBucket(void* args);
+void* sortBucket(void* args);
 void quickSort(int* bucket, int low, int high);
 int partition(int* bucket, int low, int high);
 
 void bucketSort(int array[], int array_size, int num_threads){
 	//Init bucket list
-	int num_buckets = (num_threads) ? num_threads : 10;
+	int num_buckets = (num_threads) ? num_threads : 5;
 	//int bucket_terminator = RAND_MAX+1;
 	struct Bucket *bucket = (struct Bucket*) calloc(num_buckets, sizeof(struct Bucket));
 	for (int n = 0; n < num_buckets; ++n){
@@ -49,15 +49,7 @@ void bucketSort(int array[], int array_size, int num_threads){
 		bucket[dest_bucket].bucket[bucket[dest_bucket].size] = array[i];
 		bucket[dest_bucket].size += 1;
 
-	}/*
-	for (int i = 0; i < num_buckets; ++i){
-		for (int j = 0; j < bucket[i].size; ++j){
-			printf("%d ", bucket[i].bucket[j]);
-		}
-		printf(" %d\n", mpi_rank);
-		free(bucket[i].bucket);
-		//free((void *) &bucket[i]);
-	}*/
+	}
 
 	//use pthreads to sort the buckets
 	pthread_t tid[num_threads];
@@ -65,61 +57,88 @@ void bucketSort(int array[], int array_size, int num_threads){
 		for (int p = 0; p < num_buckets; ++p){
 			pthread_create(&tid[p], NULL, sortBucket, (void*) &bucket[p]);
 		}
-	}
-	else { //Main thread sorts over array and then sorts all buckets
-		for (int n = 0; n < num_buckets; ++n){
-			sortBucket((void*) &bucket[n]);
-		}
-	}
-
 	
-	//pass sorted buckets to threads of rank 0 for combining and final sorting
-	if (mpi_rank != 0){
-		//MPI_Barrier(MPI_COMM_WORLD);
-		//package and send bucket data
-		for (int i = 0; i < num_buckets; ++i){
-			MPI_Send(&bucket[i].size, 1, MPI_INT, 0, (i+1)*mpi_rank+1, MPI_COMM_WORLD);
+		//pass sorted buckets to threads of rank 0 for combining and final sorting
+		if (mpi_rank != 0){
+			//MPI_Barrier(MPI_COMM_WORLD);
+			MPI_Request request;
+			//package and send bucket data
+			for (int i = 0; i < num_buckets; ++i){
+				MPI_Isend(&bucket[i].size, 1, MPI_INT, 0, (i+1)*mpi_rank+1, MPI_COMM_WORLD, &request);
+			}
+			//MPI_Barrier(MPI_COMM_WORLD);
+			for (int i = 0; i < num_buckets; ++i){
+				MPI_Isend(bucket[i].bucket, bucket[i].size, MPI_INT, 0, (i+1)*mpi_rank, MPI_COMM_WORLD, &request);
+			}
 		}
-		MPI_Barrier(MPI_COMM_WORLD);
-		for (int i = 0; i < num_buckets; ++i){
-			MPI_Send(bucket[i].bucket, bucket[i].size, MPI_INT, 0, (i+1)*mpi_rank, MPI_COMM_WORLD);
-		}
-	}
-	if (num_threads){
 		for (int i = 0; i < num_threads; ++i){
 			pthread_detach(tid[i]);
 		}
-		if (mpi_rank == 0){
-			for (int i = 0; i < num_buckets; ++i){
-				for (int j = 0; j < bucket[i].size; ++j){
-					printf("%d ", bucket[i].bucket[j]);
-				}
-				printf("\n");
-			}	
-		}
-		
 	}
-	
+	else { //Main thread sorts over array and then sorts all buckets
+		//sortBucket((void*) &bucket[n]);
+		MPI_Request request;
+		if (mpi_rank != 0){
+			for (int i = 0; i < num_buckets; ++i){
+				quickSort(bucket[i].bucket, 0, bucket[i].size-1);
+				MPI_Isend(&bucket[i].size, 1, MPI_INT, 0, (i+1)*mpi_rank+1, MPI_COMM_WORLD, &request);
+				MPI_Isend(bucket[i].bucket, bucket[i].size, MPI_INT, 0, (i+1)*mpi_rank, MPI_COMM_WORLD, &request);
+				MPI_Barrier(MPI_COMM_WORLD);
+			}
+		}
+		else {
+			for (int i = 0; i < num_buckets; ++i){
+				quickSort(bucket[i].bucket, 0, bucket[i].size-1);
+				int size = 0;
+				int sizes[mpi_size];
+				sizes[0] = bucket[i].size;
+				int* recv_bucket;
+				MPI_Status status;
+				for (int j = 1; j < mpi_size; ++j){
+					int tmp_size;
+					MPI_Irecv(&tmp_size, 1, MPI_INT, j, (i+1)*j+1, MPI_COMM_WORLD, &request);
+					MPI_Wait(&request, &status);
+					size += tmp_size;
+					sizes[j] = tmp_size;
+				}
+				
+				recv_bucket = (int*) calloc(size, sizeof(int));
 
+				for (int j = 1; j < mpi_size; ++j){
+					//tmp_bucket = (int*) calloc(sizes[j], sizeof(int));
+					MPI_Irecv(recv_bucket+sizes[j-1], sizes[j], MPI_INT, j, (i+1)*j, MPI_COMM_WORLD, &request);
+					MPI_Wait(&request, &status);
+				}
+				quickSort(recv_bucket, 0, size-1);
+				free(bucket[i].bucket);
+				bucket[i].bucket = recv_bucket;
+				bucket[i].size = size;
+				bucket[i].size_actual = size;
+				MPI_Barrier(MPI_COMM_WORLD);
+			}				
+		
+		}
+
+	}
 }
 
-void sortBucket(void* args){
+void* sortBucket(void* args){
 	struct Bucket bucket = *(struct Bucket*)args;
 	quickSort(bucket.bucket, 0, bucket.size-1);
 	int rank = 1;
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	if (rank == 0){
-		MPI_Barrier(MPI_COMM_WORLD);
+		//MPI_Barrier(MPI_COMM_WORLD);
 		//receive mpi_size buckets per thread
 		int size = 0;
 		int sizes[mpi_size];
 		sizes[0] = bucket.size;
 		int* recv_bucket;
-		MPI_Status status;
+		MPI_Request request;
 		//start by getting lengths of buckets
 		for (int j = 1; j < mpi_size; ++j){
 			int tmp_size;
-			MPI_Recv(&tmp_size, 1, MPI_INT, j, (bucket.num-1)*j+1, MPI_COMM_WORLD, &status);
+			MPI_Irecv(&tmp_size, 1, MPI_INT, j, (bucket.num+1)*j+1, MPI_COMM_WORLD, &request);
 			size += tmp_size;
 			sizes[j] = tmp_size;
 		}
@@ -127,7 +146,7 @@ void sortBucket(void* args){
 		//int* tmp_bucket;
 		for (int j = 1; j < mpi_size; ++j){
 			//tmp_bucket = (int*) calloc(sizes[j], sizeof(int));
-			MPI_Recv(recv_bucket+sizes[j-1], sizes[j], MPI_INT, j, (bucket.num-1)*j, MPI_COMM_WORLD, &status);
+			MPI_Irecv(recv_bucket+sizes[j-1], sizes[j], MPI_INT, j, (bucket.num+1)*j, MPI_COMM_WORLD, &request);
 		}
 		//recv_bucket now contains all buckets from other ranks
 		quickSort(recv_bucket, 0, size-1);
@@ -137,6 +156,7 @@ void sortBucket(void* args){
 		bucket.size = size;
 		bucket.size_actual = size;
 	}
+	return NULL;
 }
 
 void quickSort(int* bucket, int low, int high){
@@ -167,30 +187,22 @@ int partition(int* bucket, int low, int high){
 	return i+1;
 }
 
+/*
 int main(int argc, char *argv[]) {
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
 
-	/*int a = 0;
-    //char hostname[256];
-    //gethostname(hostname, sizeof(hostname));
-    printf("PID %d ready for attach\n", getpid());
-    fflush(stdout);
-    while (0 == a)
-        sleep(5);
-	*/
 	int array[100];
 	for (int i = 0; i < 100; ++i){
-		array[i] = 100-i;
+		array[i] = 99-i;
 	}
 	
-	bucketSort(array, 100, 5);
+	bucketSort(array, 100, 4);
 	
-	MPI_Barrier(MPI_COMM_WORLD);
-	MPI_Finalize();
+	//MPI_Barrier(MPI_COMM_WORLD);
+	//MPI_Finalize();
 	return 0;
 }
-
-
+*/
 
